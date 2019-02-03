a0dc7bc98c32a036da534a9a96e81ab398f3e3df
TJ Bearse <thomasb@qualtrics.com>
2019-02-02 18:01:55 -0800

    colorized cards

--- src/myActions.js
+++ src/myActions.js
@@ -2,13 +2,18 @@ import {TextAction} from './model/actions';
 import {Die, Constant, Fixed, Max, Min, CountDown, Even, Odd} from './model/dice';
 
 const Actions = [
-	new TextAction("+1 Sword", "a", [new Die(), new Constant(3)], "[ ] Damage"),
-	new TextAction("Sword of Testing", "b", [new Max(3)], "1 Damage", "Reusable Twice"),
-	new TextAction("Sword of Testing", "c", [new CountDown(7)], "1 Damage"),
-	new TextAction("Sword of Double Testing", "a", [new Even(2), new Fixed(3)], "4 Damage"),
-	new TextAction("Sword of Triple Testing", "b", [new Odd("7"), new Min(3), new Min(3)], "4 Damage"),
-	new TextAction("Sword of a Fuckton of Testing", "c", [...Array(5).keys()].map(() => new Die("7")), "Insta death"),
-	new TextAction("Boots of Fixed Values", "d", [...Array(6).keys()].map((i) => new Fixed(i+1)), "4 D, typeamage"),
+	new TextAction("Sword of a Fuckton of Testing", "a", [...Array(5).keys()].map(() => new Die("7")), "Insta death"),
+	new TextAction("Sword of Testing", "b", [new CountDown(7)], "1 Damage"),
+	new TextAction("Sword of Double Testing", "c", [new Even(2), new Fixed(3)], "4 Damage"),
+
+
+	new TextAction("Vorpal Sword", "a", [new Die(), new Constant(3), new Fixed(4), new Max(3), new Odd(), new CountDown(3)], "[ ] Damage", "Reuse all the time"),
+	new TextAction("Vorpal Sword", "b", [new Die(), new Constant(3), new Fixed(4), new Min(3), new Odd(), new CountDown(3)], "[ ] Damage", "Reuse all the time"),
+	new TextAction("Vorpal Sword", "c", [new Die(), new Constant(3), new Fixed(4), new Max(3), new Odd(), new CountDown(3)], "[ ] Damage", "Reuse all the time"),
+
+	new TextAction("Boots of Fixed Values", "a", [...Array(6).keys()].map((i) => new Fixed(i+1)), "4 D, typeamage"),
+	new TextAction("Boots of Fixed Values", "b", [...Array(6).keys()].map((i) => new Fixed(i+1)), "4 D, typeamage"),
+	new TextAction("Boots of Fixed Values", "c", [...Array(6).keys()].map((i) => new Fixed(i+1)), "4 D, typeamage"),
 ]
 
 export default Actions
